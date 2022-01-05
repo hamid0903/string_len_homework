83f04acbed5c9da05cc6fd8d2abefdf81830816d
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-05 22:50:45 +0500

    len07-1

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,20 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    a=len(s1)%2
+    b=len(s2)%2
+    c=len(s3)%2
+    ans=""
+    x1="["
+    x2="]"
+    if a!=1:
+        ans=x1+s2+s3+x2
+    if b!=1:
+        ans=x1+s1+s3+x2
+    if c!=1:
+        ans=x1+s1+s2+x2
+    else:
+        x1+x2
+
+    return ans
+print(main("ab", "abc", "abcd"))
